728592146fa39645ae6edf442651243d2f3db11e
Yuriy Demchenko <deuriy@yandex.ru>
2020-06-23 17:27:55 +0300

    Change popup adding company

--- src/scripts/companies.js
+++ src/scripts/companies.js
@@ -1,16 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 	$('[data-src="#CompanyAdding"]').fancybox({
 		touch: false,
-		baseTpl:
-	    '<div class="fancybox-container fancybox-container--no-padding" role="dialog" tabindex="-1">' +
-	    '<div class="fancybox-bg"></div>' +
-	    '<div class="fancybox-inner">' +
-	    '<div class="fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' +
-	    '<div class="fancybox-toolbar">{{buttons}}</div>' +
-	    '<div class="fancybox-navigation">{{arrows}}</div>' +
-	    '<div class="fancybox-stage"></div>' +
-	    '<div class="fancybox-caption"><div class=""fancybox-caption__body"></div></div>' +
-	    '</div>' +
-	    '</div>',
+		baseClass: 'fancybox-container--no-padding'
 	});
 });
