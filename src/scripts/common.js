b1cf9a8f197ce40aec173c5db9e49478d85d98fd
Yuriy Demchenko <deuriy@yandex.ru>
2020-06-27 22:06:16 +0300

    Script editing

--- src/scripts/common.js
+++ src/scripts/common.js
@@ -748,7 +748,38 @@ document.addEventListener("DOMContentLoaded", function () {
 		rating.querySelector('.Rating_dropdown').classList.remove('Rating_dropdown-visible');
 	});
 
+	// Rating line fill
 	let ratingLineFill = document.querySelectorAll('.ExtendedRating_lineFill');
 	ratingLineFill.forEach( el => el.style.width = (el.dataset.mark * 10) + '%');
+
+	document.addEventListener('click', function (e) {
+		let closeRemark = e.target.closest('.Remark_close');
+
+		if (!closeRemark) return;
+
+		closeRemark.closest('.Remark').classList.add('hidden');
+	});
+
+	// Open/close control dropdown
+	document.addEventListener('click', function (e) {
+		let controlBtn = e.target.closest('.Control_btn');
+
+		if (!controlBtn) return;
+
+		let controlMenu = controlBtn.parentNode.querySelector('.Control_menu');
+		controlMenu.classList.toggle('Control_menu-opened');
+	});
+
+	document.addEventListener('click', function (e) {
+		let controlMenu = document.querySelector('.Control_menu');
+
+		if (!controlMenu) return;
+
+		let controlBtn = controlMenu.parentNode.querySelector('.Control_btn');
+
+		if (controlMenu.classList.contains('Control_menu-opened') && !controlMenu.contains(e.target) && !controlBtn.contains(e.target)) {
+			controlMenu.classList.remove('Control_menu-opened');
+		}
+	});
 
 });
