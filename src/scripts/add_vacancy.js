e00c915a77a23e69925364182394623b11b2cd76
Yuriy Demchenko <deuriy@yandex.ru>
2020-09-19 23:31:52 +0300

    Edits on the add vacancy page

--- src/scripts/add_vacancy.js
+++ src/scripts/add_vacancy.js
@@ -1,22 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
 	function changeSalaryType () {
 		let salaryType = document.querySelector('select[name="salary_type"]');
+		let salaryField = document.querySelector('.CompanySettings_field-salary');
 		let salaryRange = document.querySelector('.NumberField-salaryRange');
 		let salaryRangeFrom = salaryRange.querySelector('.NumberField_formText-from');
 		let salaryRangeTo = salaryRange.querySelector('.NumberField_formText-to');
 		let salaryRangeFixed = salaryRange.querySelector('.NumberField_formText-fixedSalary');
 		let salaryRangeSeparator = salaryRange.querySelector('.NumberField_rangeSeparator');
+		let currencySelect = salaryRange.querySelector('.NumberField_currencySelect select');
 
-		if (salaryType.value === 'fixed') {
-			salaryRangeFrom.hidden = salaryRangeTo.hidden = salaryRangeSeparator.hidden = true;
-			salaryRangeFrom.disabled = salaryRangeTo.disabled = true;
-			salaryRangeFixed.hidden = false;
-			salaryRangeFixed.disabled = false;
-		} else if (salaryType.value === 'flexible') {
-			salaryRangeFrom.hidden = salaryRangeTo.hidden = salaryRangeSeparator.hidden = false;
-			salaryRangeFrom.disabled = salaryRangeTo.disabled = false;
-			salaryRangeFixed.hidden = true;
-			salaryRangeFixed.disabled = true;
+		if (salaryField.hidden) {
+			salaryType.disabled = salaryRangeFrom.disabled = salaryRangeTo.disabled = salaryRangeFixed.disabled = currencySelect.disabled = true;
+			return;
+		}
+
+		salaryType.disabled = currencySelect.disabled = false;
+
+		switch (salaryType.value) {
+			case 'fixed':
+				salaryRangeFrom.hidden = salaryRangeTo.hidden = salaryRangeSeparator.hidden = true;
+				salaryRangeFrom.disabled = salaryRangeTo.disabled = true;
+				salaryRangeFixed.hidden = false;
+				salaryRangeFixed.disabled = false;
+				break;
+			case 'flexible':
+				salaryRangeFrom.hidden = salaryRangeTo.hidden = salaryRangeSeparator.hidden = false;
+				salaryRangeFrom.disabled = salaryRangeTo.disabled = false;
+				salaryRangeFixed.hidden = true;
+				salaryRangeFixed.disabled = true;
+				break;
+		}
+	}
+
+	function changeVacancyType () {
+		let vacancyType = document.querySelector('select[name="vacancy_type"]');
+		let salaryField = document.querySelector('.CompanySettings_field-salary');
+
+		switch (vacancyType.value) {
+			case 'free':
+				salaryField.hidden = true;
+				break;
+			case 'paid':
+				salaryField.hidden = false;
+				break;
 		}
 	}
 
@@ -28,6 +54,28 @@ document.addEventListener('DOMContentLoaded', function () {
 		changeSalaryType();
 	});
 
+	document.addEventListener('click', function (e) {
+		let vacancyTypeDropdown = e.target.closest('.DropdownSelect-vacancyType');
+
+		if (!vacancyTypeDropdown) return;
+
+		changeVacancyType();
+		changeSalaryType();
+	});
+
+	// document.addEventListener('click', function (e) {
+	// 	let dropdownSelect = e.target.closest('.DropdownSelect');
+
+	// 	if (!dropdownSelect) return;
+		
+	// 	dropdownSelect.querySelector('.DropdownSelect_select').dispatchEvent(new Event('change'));
+	// });
+
+	// document.querySelector('select[name="vacancy_type"]').addEventListener('change', function () {
+	// 	console.log('Change!!!');
+	// });
+
+	changeVacancyType();
 	changeSalaryType();
 	
 });
