3c42ddb475bfdbb8062c88bf5c8217b9ed6b6adf
deuriy <deuriy@yandex.ru>
2021-09-01 21:33:50 +0300

    Plans page edits, hint edits

--- src/scripts/plans.js
+++ src/scripts/plans.js
@@ -3,6 +3,37 @@ const plansSwiper = new Swiper('.PlansSwiper', {
   spaceBetween: 12
 });
 
+function checkHintPosition (hint) {
+  if (!hint) return;
+  
+  let hintIcon = hint.querySelector('.Hint_icon');
+  let hintWrapper = hint.querySelector('.Hint_wrapper');
+
+  if (!hintIcon || !hintWrapper) return;
+
+  hint.classList.remove('Hint-top', 'Hint-bottom', 'Hint-left', 'Hint-largeWidth');
+
+  let coords = hintIcon.getBoundingClientRect();
+  let windowHeight = document.documentElement.clientHeight;
+  let offsetBottom = windowHeight - coords.bottom - hintIcon.offsetHeight - hintWrapper.offsetHeight - 20;
+  let offsetTop = hintIcon.getBoundingClientRect().top - hintWrapper.offsetHeight;
+  let offsetLeft = hintWrapper.getBoundingClientRect().left;
+
+  if (offsetBottom < 0 && offsetTop < 0) {
+    hint.classList.add('Hint-largeWidth');
+  }
+
+  if (offsetBottom < 0) {
+    hint.classList.add('Hint-top');
+  } 
+
+  if (offsetLeft < 0 || hint.classList.contains('Hint-largeWidth')) {
+    hint.classList.add('Hint-left');
+  }
+}
+
+document.querySelectorAll('.Hint').forEach(checkHintPosition);
+
 // Hint
 document.addEventListener('mouseover', function (e) {
   let hint = e.target.closest('.Hint');
@@ -10,6 +41,7 @@ document.addEventListener('mouseover', function (e) {
   if (!hint) return;
 
   hint.classList.add('Hint-opened');
+  checkHintPosition(hint);
 
   e.preventDefault();
 });
@@ -19,17 +51,6 @@ document.addEventListener('mouseout', function (e) {
 
   if (!hint) return;
 
-  hint.classList.remove('Hint-opened');
-
-  e.preventDefault();
-});
-
-document.addEventListener('click', function (e) {
-  let hintClose = e.target.closest('.Hint_close');
-
-  if (!hintClose) return;
-
-  let hint = hintClose.closest('.Hint');
   hint.classList.remove('Hint-opened');
 
   e.preventDefault();
